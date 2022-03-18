3f74464f645222f28e96d84899926c5d0962a2cb
khoon0608 <khoon0608@gmail.com>
2022-03-18 19:29:24 +0900

    주사위 추가, 제거 버튼 추가

--- src/components/game.js
+++ src/components/game.js
@@ -1,25 +1,48 @@
 /** @format */
+import React from "react";
 
 import Dice from "./dice";
 
 export default function Game() {
+  const [diceState, setDiceState] = React.useState({
+    number: 1,
+    isFreeze: false,
+  });
+
+  const [diceArr, setDiceArr] = React.useState([]);
+
+  const diceComponents = diceArr.map((item, index) => (
+    <Dice key={index} state={item} />
+  ));
+
+  function addDice() {
+    setDiceArr((prevDiceArr) => [diceState, ...prevDiceArr]);
+  }
+
+  function deleteDice() {
+    if (diceArr.length > 0) {
+      setDiceArr((prevDiceArr) => {
+        let newDiceArr = [...prevDiceArr];
+        newDiceArr.pop();
+        return newDiceArr;
+      });
+    } else {
+      alert("제거 할 주사위가 없습니다.");
+    }
+  }
+
   return (
     <section className='game'>
-      <div className='game--dice__wrap'>
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-        <Dice />
-      </div>
+      <div className='game--dice__wrap'>{diceComponents}</div>
 
       <div className='game--btn'>
         <span>Roll</span>
+      </div>
+      <div className='game--btn' onClick={addDice}>
+        <span>add</span>
+      </div>
+      <div className='game--btn' onClick={deleteDice}>
+        <span>delete</span>
       </div>
     </section>
   );
