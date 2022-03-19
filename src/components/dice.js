56b1f0429865d7422dccb3b1e0cc922837e47760
khoon0608 <khoon0608@gmail.com>
2022-03-19 12:40:49 +0900

    완료

--- src/components/dice.js
+++ src/components/dice.js
@@ -1,12 +1,12 @@
 /** @format */
 
-export default function Dice({ state, clickEvent, hoverEvent }) {
+export default function Dice({ state, clickEvent,  }) {
   return (
     <div
       className='dice'
       onClick={clickEvent}
-      onMouseEnter={hoverEvent}
       id={state.id}
+      value={state.number}
       style={state.isFreeze ? { background: "rgb(89, 227, 145)" } : null}
       isfreeze={state.isFreeze.toString()}>
       {state.number}
--- src/components/game.js
+++ src/components/game.js
@@ -4,32 +4,28 @@ import React from "react";
 import Dice from "./dice";
 
 export default function Game() {
-  const [currentDiceId, setCurrentDiceId] = React.useState(null);
-
   const [diceArr, setDiceArr] = React.useState([]);
 
+  const [isGameOver, setIsGameOver] = React.useState(null);
+
+  React.useEffect(() => {
+    setIsGameOver(false);
+  }, []);
+
   const diceComponents = diceArr.map((item, index) => (
-    <Dice
-      key={index}
-      state={item}
-      clickEvent={doFreezeDice}
-      hoverEvent={findCurrnetDice}
-    />
+    <Dice key={index} state={item} clickEvent={doFreezeDice} />
   ));
 
-  function addDice(event) {
-    event.preventDefault();
+  function addDice() {
     const diceState = {
       id: Math.random(),
       number: Math.ceil(Math.random() * 6),
       isFreeze: false,
     };
     setDiceArr((prevDiceArr) => [...prevDiceArr, diceState]);
-    setCurrentDiceId(diceState.id);
   }
 
-  function deleteDice(event) {
-    event.preventDefault();
+  function deleteDice() {
     if (diceArr.length > 0) {
       setDiceArr((prevDiceArr) => {
         let newDiceArr = [...prevDiceArr];
@@ -37,20 +33,14 @@ export default function Game() {
         return newDiceArr;
       });
     } else {
-      alert("제거 할 주사위가 없습니다.");
+      alert("제거할 주사위가 없습니다.");
     }
   }
 
-  function findCurrnetDice(event) {
-    setCurrentDiceId((prevDiceId) =>
-      prevDiceId !== event.target.id ? event.target.id : prevDiceId
-    );
-  }
-
-  function doFreezeDice() {
+  function doFreezeDice(event) {
     setDiceArr((prevDiceArr) =>
       prevDiceArr.map((dice) => {
-        return dice.id === +currentDiceId
+        return dice.id === +event.target.id
           ? {
               ...dice,
               isFreeze: !dice.isFreeze,
@@ -60,8 +50,7 @@ export default function Game() {
     );
   }
 
-  function rollDice(event) {
-    event.preventDefault();
+  function rollDice() {
     setDiceArr((prevDiceArr) =>
       prevDiceArr.map((dice) =>
         dice.isFreeze ? dice : { ...dice, number: Math.ceil(Math.random() * 6) }
@@ -69,20 +58,43 @@ export default function Game() {
     );
   }
 
+  function reset() {
+    setDiceArr([]);
+    setIsGameOver(false);
+  }
+
+  if (
+    diceArr.length &&
+    isGameOver === false &&
+    !diceArr.find(
+      (dice) => dice.isFreeze == false || +dice.number !== +diceArr[0].number
+    )
+  ) {
+    setIsGameOver(true);
+  }
+
   return (
-    <form className='game'>
+    <main className='game'>
       <div className='game--dice__wrap'>{diceComponents}</div>
-      <section className='game--btn__wrap'>
-        <button className='game--btn' onClick={addDice}>
-          <span>add</span>
-        </button>
-        <button className='game--btn'>
-          <span onClick={rollDice}>Roll</span>
-        </button>
-        <button className='game--btn' onClick={deleteDice}>
-          <span>delete</span>
-        </button>
-      </section>
-    </form>
+      {isGameOver ? (
+        <section className='game--btn__wrap'>
+          <button className='game--btn' onClick={reset}>
+            <span>reset</span>
+          </button>
+        </section>
+      ) : (
+        <section className='game--btn__wrap'>
+          <button className='game--btn' onClick={addDice}>
+            <span>add</span>
+          </button>
+          <button className='game--btn'>
+            <span onClick={rollDice}>Roll</span>
+          </button>
+          <button className='game--btn' onClick={deleteDice}>
+            <span>delete</span>
+          </button>
+        </section>
+      )}
+    </main>
   );
 }
